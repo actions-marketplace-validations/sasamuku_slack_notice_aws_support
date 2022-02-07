959e545cca62d1a0ce4af7e6b4a9372af147fb9c
sasamuku <sasazawa3330@gmail.com>
2022-02-07 21:46:49 +0900

    add GetCases

--- aws/support.go
+++ aws/support.go
@@ -2,14 +2,29 @@ package aws
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/support"
 )
 
-func GetCases() {
+type Case struct {
+	Subject    string `json:"subject"`
+	Status     string `json:"status"`
+	SubmitteBy string `json:"submitteBy"`
+	Url        string `json:"url"`
+}
+
+func NewDescribeCasesInput(aftertime, beforetime, language string) *support.DescribeCasesInput {
+	return &support.DescribeCasesInput{
+		AfterTime:            &aftertime,
+		BeforeTime:           &beforetime,
+		Language:             &language,
+		IncludeResolvedCases: true,
+	}
+}
+
+func GetCases(input *support.DescribeCasesInput) *[]Case {
 	// Load the Shared AWS Configuration (~/.aws/config)
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
@@ -20,10 +35,27 @@ func GetCases() {
 
 	client := support.NewFromConfig(cfg)
 
-	output, err := client.DescribeCases(context.TODO(), &support.DescribeCasesInput{})
+	output, err := client.DescribeCases(context.TODO(), input)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(output)
+	cases := ArrangeCases(output)
+	return cases
+}
+
+func ArrangeCases(output *support.DescribeCasesOutput) *[]Case {
+	cases := []Case{}
+
+	outputCases := output.Cases
+	for _, c := range outputCases {
+		eachCase := Case{
+			Subject:    *c.Subject,
+			Status:     *c.Status,
+			SubmitteBy: *c.SubmittedBy,
+			Url:        "",
+		}
+		cases = append(cases, eachCase)
+	}
+	return &cases
 }
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/sasamuku/aws_support_to_github_issue/aws"
 )
 
 func main() {
-	aws.GetCases()
+	aftertime := "2021-11-06T01:27:05.739Z"
+	beforetime := "2022-01-06T01:27:05.739Z"
+	language := "ja"
+
+	input := aws.NewDescribeCasesInput(aftertime, beforetime, language)
+	cases := aws.GetCases(input)
+
+	for _, c := range *cases {
+		fmt.Println(c.Subject)
+	}
 }
